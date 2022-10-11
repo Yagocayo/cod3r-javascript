4e8298852b021e262d125033cf19c964a6a5e53b
yago.cayo31@gmail.com <108165037+Yagocayo@users.noreply.github.com>
2022-10-11 20:39:46 -0300

    operadores relacionais

---
+++ operadores/relacionais_2.js
@@ -0,0 +1,11 @@
+let a = 1
+let b = "1"
+let c = "1"
+
+console.log(a == b) //sinal de igual. aqui se compara se os conteudos sao iguais
+console.log(a === b) //sinal de restritamente iguais.  aqui se compara se o tipos sao restritamente iguais (valores string , numerais e etc)
+console.log(b === c)
+
+console.log(a != b)//sinal de diferente. aqui se compara se os conteudos sao diferentes
+console.log(a !== b)//sinal de restritamente diferentes.  aqui se compara se o tipos sao restritamente diferentes (valores string , numerais e etc)
+console.log(b !== c)
