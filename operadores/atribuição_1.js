265bcbbdb6678af92e59a752d0672ddaf23b6407
yago.cayo31@gmail.com <108165037+Yagocayo@users.noreply.github.com>
2022-10-11 21:50:46 -0300

    atribuição

---
+++ operadores/atribuição_1.js
@@ -0,0 +1,20 @@
+let x = 10
+x = x + 6 //somando um novo valor a variavel
+x = x - 1
+
+let y = 20                //atribuição normal
+y += 10 // y = y + 10     //atribuição adtivva
+y -= 2  // y = y - 2     //atribuição subtrativa
+y *= 2  // y = y * 2     //atribuição multiplicatifa
+y /= 4  // y = y / 4     //atribuição divisiva
+y %= 3  // y = y % 3    //atribuição modulo de divisão(resto de divisão)
+
+console.log(x)
+console.log(y)
+
+let w = "texto"
+
+w += ", mais texto"
+w += '!!!'
+
+console.log(w)
