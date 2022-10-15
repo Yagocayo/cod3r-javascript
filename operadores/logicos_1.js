4af70cff09878b531896de8cd47eb601e480ee6c
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2022-10-14 21:47:05 -0300

    operadores logicos

---
+++ operadores/logicos_1.js
@@ -0,0 +1,26 @@
+let temDinheiro = false
+let estaEnsolarado = false
+let carroEstaNaGaragem = false
+
+let resultadoE = '(AND) 1- vai pro shopping?'
+resultadoE += temDinheiro && estaEnsolarado
+
+console.log(resultadoE)
+
+let resultadoOU = '2- vai pro shopping?'
+resultadoOU += estaEnsolarado || carroEstaNaGaragem
+console.log(resultadoOU)
+
+console.log(true !== true)
+console.log(true !== false)
+console.log(false !== true)
+console.log(false !== false)
+
+console.log('não verdadeiro: '! true)
+console.log('nao falso' + !false)
+
+/*
+E, AND = &&
+OU, OR = ||
+OU exclusivo, xor = !==
+*/
