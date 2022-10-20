08c67cdfe555503eff5e961318a4119a5851766b
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2022-10-19 21:55:05 -0300

    finalizando operadores unários

---
+++ operadores/unarios_3.js
@@ -0,0 +1,12 @@
+console.log(!!true)
+console.log(!!false)
+
+console.log(!!'texto!')
+console.log(!!'')
+
+console.log(!!1)
+console.log(!!0)
+console.log(!!-1)
+console.log(!!-0.00001)
+
+// todos os numeros são verdadeiros no teste apenas o zero e falso
