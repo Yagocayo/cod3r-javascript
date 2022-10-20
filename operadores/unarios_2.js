c4a551af1da02cf3a61eaf64f05ea3676a0dcbdf
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2022-10-19 21:35:57 -0300

    .

---
+++ operadores/unarios_2.js
@@ -0,0 +1,8 @@
+let a = 1
+let b = 2
+
+//console.log(++a === b++) presedencia do ++a e maior que o b++ e o sinal de igualdade opera primeiro que o incremento do b++
+
+a++
+console.log(a === b)
+b++
