f957a12695369e61d6e15ac4f286051ac9f2b924
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2022-11-11 19:53:20 -0300

    .

--- Estruturas-de-controle/for_1.js
+++ Estruturas-de-controle/for_1.js
@@ -1,9 +1,8 @@
 
-let controle = 1;
 
 //      declaração       expresão      incrementação
 for (let controle = 1; controle <= 10; controle ++) {
-    console.log(controle);
+    console.log(controle)
 }
 
 console.log('Fim!')
