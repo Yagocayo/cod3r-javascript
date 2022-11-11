77fe23bbd8fbcec1b740594b5c4bf5af506985d3
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2022-11-10 22:25:25 -0300

    estudando while

---
+++ Estruturas-de-controle/while_1.js
@@ -0,0 +1,10 @@
+// executar codigo Ctrl + Alt + N
+// parar execução do codigo Ctrl + Alt + N
+
+/* o WHILE foi criado para repetiçoes não determinadas e o FOR para repetições deterinadas apesar de ambos poderem ser usados em ambos os casos*/
+
+
+while (true){
+    console.log('laço infinito!!')
+}
+console.log('fim')
