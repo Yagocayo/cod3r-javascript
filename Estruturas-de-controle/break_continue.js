423c6b01b0d7610fca8453a3feab398705bfb1c5
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2022-11-14 23:19:08 -0300

    finalizando estruturas  de controle

---
+++ Estruturas-de-controle/break_continue.js
@@ -0,0 +1,17 @@
+// break -> interronpe o for
+for(let i = 1; i <= 5; i++) {
+    if(i === 3){
+        break
+    }
+    console.log(i)
+
+}
+// continue -> interronpe apenas a repetição e nao finaliza o laço for
+
+for(let i = 1; i <= 5; i++) {
+    if(i === 3){
+        continue
+    }
+    console.log(i)
+
+}
