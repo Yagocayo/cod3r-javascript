53d94a221b12b91cfa043f5e56c66add361c740b
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-01-11 11:47:16 -0300

    revisando funções

--- funcoes/desafio_funcao_3.js
+++ funcoes/desafio_funcao_3.js
@@ -1,23 +1,33 @@
-const nota1 = 7.8;
-const nota2 = 3.1;
-const nota3 = 2.7;
+const nota1 = 7.3;
+const nota2 = 8.9;
+const nota3 = 8;
 
-function minimo(n1, n2) {
+function minimo (n1, n2) {
     return n1 <= n2 ? n1 : n2;
-
 }
-function madia(n1, n2, n3){
-    const menorNota = minimo (n1, minimo(n2, n3));
 
-    if(menorNota === n1) {
+function media(n1, n2, n3) {
+    const menorNota = minimo(n1, minimo(n2, n3));
+
+    if(menorNota === n1){
         return (n2 + n3) / 2;
-    } else if (MenorNota === n2) {
-        return (n1 + n3 ) / 2;
+    } else if(menorNota === n2) {
+        return (n1 + n3) / 2;
     } else {
-        
+        return (n1 + n2) / 2;
     }
-
-      }
 }
 
-console.log()
+function mediaParaStatus(media) {
+    if(media >= 7){
+        return "Aprovado";
+    } else if (madia >= 4) {
+        return "Recuperacão";
+    } else {
+       return "Reprovado";
+    }     
+} 
+
+const mediaFinal = media(nota1, nota2, nota3);
+const statusFinal = mediaParaStatus(mediaFinal);
+console.log(`O resultado final do aluno é ${statusFinal}!`);
