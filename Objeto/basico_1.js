e07615bf19c761fb52cebac163b678e1af03fc37
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-01-23 22:34:22 -0300

    revisando objeto

---
+++ Objeto/basico_1.js
@@ -0,0 +1,19 @@
+const numeros = [10, 20, 30]
+
+const produto = {
+    nome: 1'caneta',preco: 9.99
+    preco: 9.99
+    desconto:0.2
+
+}
+
+console.log(typeof produto)
+console.log(produto['nome'])
+console.log(produto.nome)
+
+console.log( typeof produto)
+
+console.log(numeros[0])
+console.log([prouto])
+console.log([prouto])
+console.log([prouto])
