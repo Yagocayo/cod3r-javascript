9a3a8dcce8e9c6efba407e4bdf1d12213aa3cda3
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-02-01 12:54:07 -0300

    revisando objeto

--- Objeto/basico_1.js
+++ Objeto/basico_1.js
@@ -1,19 +1,31 @@
-const numeros = [10, 20, 30]
+/*let x = 3
+let y = []
+
+console.log(Array.isArray(x))
+
+
 
 const produto = {
-    nome: 1'caneta',preco: 9.99
-    preco: 9.99
-    desconto:0.2
-
+    nome: 'caneta'
 }
+console.log(typeof produto)*/
+
+//object
+
+const numeros = [10, 20, 30]
+const produto = {
+     nome: 'Caneta',
+     preco: 9.99,
+     desconto: 0.2
+}
+console.log(numeros[0])
+console.log(produto['nome'])
+console.log(produto['preco'])
+console.log(produto['desconto'])
+
+
+console.log(produto.nome)
+console.log(numeros.preco)
+console.log(numeros.desconto)
 
 console.log(typeof produto)
-console.log(produto['nome'])
-console.log(produto.nome)
-
-console.log( typeof produto)
-
-console.log(numeros[0])
-console.log([prouto])
-console.log([prouto])
-console.log([prouto])
