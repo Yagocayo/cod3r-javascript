7b6c2c3f3bc0abf320fa3b56e4ce25fb0c9d90d3
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-05-08 20:53:33 -0300

    Update atribuicao_desafio.js

--- cod3r-javascript/operadores/atribuicao_desafio.js
+++ cod3r-javascript/operadores/atribuicao_desafio.js
@@ -1,9 +1,10 @@
-// para quebrar a linha se usa o \nce para dar um tab espaço \t
+// para quebrar a linha se usa o \n ce para dar um tab espaço \t
 
 let texto = ' Lista de Aprovados \n ================== \n'
+
 texto += '\n 1. Ana Silva'
 texto += '\n 2. Pedro Albuguerque'
 texto += '\n 3. Guilherme pereira'
 texto += '\n 4. Rebeca França'
 
-console.log(texto)
+console.log(texto)//
