54d9e923fe686b885e831e43f4e772e2c71ceb9c
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-05-24 23:44:09 -0300

    .

--- Estruturas-de-controle/while_1.js
+++ Estruturas-de-controle/while_1.js
@@ -1,5 +1,5 @@
 // executar codigo Ctrl + Alt + N
-// parar execução do codigo Ctrl + Alt + N
+// parar execução do codigo Ctrl + Alt + m
 
 /* o WHILE foi criado para repetiçoes não determinadas e o FOR para repetições deterinadas apesar de ambos poderem ser usados em ambos os casos*/
 
