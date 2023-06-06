7740a28acbbda9954eef72f1300dfe5e60b8b76a
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-06-05 21:50:19 -0300

    revisando funções

--- funcoes/desafio_funcao_1.js
+++ funcoes/desafio_funcao_1.js
@@ -5,9 +5,21 @@ function somar (n1 , n2){
 function subtrair (a1, a2){
     console.log(a1 - a2)
 }
+function multiplicar (aa1, bb2, cc3) {
+    console.log(aa1 * bb2 * cc3)
+}
+function dividir (a, b, c, d) {
+    console.log(a /b /c /d)
+}
 
 somar(3,4)
 somar(10,4)
 
 subtrair(3,2)
 subtrair(10,25)
+
+multiplicar ( 2, 2, 10)
+multiplicar ( 2, 2, 2)
+
+dividir(1000, 10, 5, 4)
+dividir(10000, 5, 5, 4)
