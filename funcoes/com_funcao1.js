ac2f8eb6526b0fbaba276e0d2b012509d4f36f6c
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-07-06 06:20:43 -0300

    revisando funcoes

--- funcoes/com_funcao1.js
+++ funcoes/com_funcao1.js
@@ -13,8 +13,27 @@ const a2 = 2022
 
 function exibirData2() {
     console.log(`Dia: ${d2}/${m2}/${a2}`)
-    
 }
 
-exibirData1()
-exibirData2() 
+const d3 = 10
+const m3 = 7
+const a3 = 2021
+    
+function exibirData3() {
+    console.log(`Dia: ${d3}/${m3}/${a3}`)
+        
+}
+    
+const d4 = 15
+const m4 = 12
+const a4 = 2022
+
+function exibirData4() {
+    console.log(`Dia: ${d4}/${m4}/${a4}`)
+
+}
+
+exibirData1();
+exibirData2(); 
+exibirData3();
+exibirData4(); 
