4317c9529ca9863e70a35bb5cbf492d28200900d
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-07-14 07:13:17 -0300

    revisano funcoes

--- funcoes/desafio_funcao_3.js
+++ funcoes/desafio_funcao_3.js
@@ -1,11 +1,11 @@
-const nota1 = 7.3;
-const nota2 = 8.9;
-const nota3 = 8;
+const nota1 = 2.3;
+const nota2 = 2;
+const nota3 = 2;
 
 function minimo (n1, n2) {
-    return n1 <= n2 ? n1 : n2;
+    return n1 <= n2 ? n1 : n2; //operador ternario
+    
 }
-
 function media(n1, n2, n3) {
     const menorNota = minimo(n1, minimo(n2, n3));
 
@@ -21,7 +21,7 @@ function media(n1, n2, n3) {
 function mediaParaStatus(media) {
     if(media >= 7){
         return "Aprovado";
-    } else if (madia >= 4) {
+    } else if (media >= 4) {
         return "Recuperacão";
     } else {
        return "Reprovado";
@@ -30,4 +30,4 @@ function mediaParaStatus(media) {
 
 const mediaFinal = media(nota1, nota2, nota3);
 const statusFinal = mediaParaStatus(mediaFinal);
-console.log(`O resultado final do aluno é ${statusFinal}!`);
+console.log(`O resultado final do aluno é ${statusFinal}!`)
