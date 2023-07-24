588dfba70c7ee1c906d6a220cd2480e7c39e5d04
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-07-24 05:28:18 -0300

    finalizando revisão do modulo de array

--- Arrays/metodos_3.js
+++ Arrays/metodos_3.js
@@ -1,5 +1,16 @@
 const numeros = [10, 20, 30, 40, 50, 60]
 
+//01 exemplo
+
+//   for (let i = 0; i < numeros.length; i++) {
+//       console.log(numeros[i]);
+//   }
+//   for (let n of numeros){
+//     console.log(n)
+//   }
+
+//02 exemplo
+
 function PraCadaElemento(elemento, indice, array) {
     console.log(elemento, indice, array)
 }
@@ -15,3 +26,5 @@ numeros.forEach(function (el) {
 numeros.forEach(function (_, i) {
     console.log(i)
 })
+
+//usando funcão dentro de parametros 
